a9cf38070363f97a6b12e8cf519e3a1930094f38
jasoninzer <112062563+jasoninzer@users.noreply.github.com>
2022-08-30 16:30:29 -0700

    Update iso_exchange.sql

--- reference_data/iso_exchange.sql
+++ reference_data/iso_exchange.sql
@@ -1,6 +1,10 @@
+/*
+Filename: iso_exchange.sql
+Description: A lookup table to store the ISO 10383 Market Identifier Codes (MICS) in Snowflake.
+Author: Jason Inzer
+Notes: These codes are updated regularly. For latest updates, see: https://www.iso20022.org/market-identifier-codes
+
+*/
 
 
-
-
-https://www.iso20022.org/market-identifier-codes
 
