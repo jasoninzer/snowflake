f73b1710c85e737741599891d6e3a30a4d5651e7
jasoninzer <112062563+jasoninzer@users.noreply.github.com>
2023-08-27 13:12:51 -0700

    Update iso_market.sql

--- reference_data/iso_market.sql
+++ reference_data/iso_market.sql
@@ -1,7 +1,7 @@
 /*
 
 Filename:     iso_market.sql
-Purpose:      Create and populate a lookup table with the ISO 10383 Market Identifier Codes (MICS).
+Purpose:      Create and populate a lookup table with the ISO 10383 Market Identifier Codes (MIC).
 Data Source:  https://www.iso20022.org/market-identifier-codes
 Notes:        The codes are updated regularly. Please refer to data source for latest.
 Database:     Snowflake
